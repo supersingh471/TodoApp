5053cf6caf93f95dffdfb52c24aac52c6800aecd
Vikash Singh <vikassingh1999999@gmail.com>
2025-09-11 02:11:18 +0530

    initialized frontend

---
+++ frontend/README.md
@@ -0,0 +1,6 @@
+# Pages for TodoApp
+
+This will have a Signup for users to create their account
+This will have a Signin page for users to login into their account
+This will have a Dashboard page for users to create and see their todo
+
---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { BrowserRoueter, Routes, Route } from "react-router-dom";
+
+function App() {
+	return (
+		<>
+		<BrowserRoueter>
+		<Routes>
+			<Route path="signup"/>
+		</Routes>
+		</BrowserRoueter>
+		</>
+	)
+}
+
+export default App
+
