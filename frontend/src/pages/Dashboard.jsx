1fca80626d84cda845554437ee78c9f006722d98
Vikash Singh <vikassingh1999999@gmail.com>
2025-09-22 02:21:57 +0530

    building dashboard p-2

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -12,11 +12,20 @@ export default function Dashboard() {
 	return (
 	  <div className="flex bg-gray-200 justify-center itmes-center h-screen p-25">
 		<div className="grid grid-cols-1 md:grid-cols-2 gap-6 w-full h-120">
-			<div className="bg-gray-900 border w-full h-full rounded-xl">
+			<div className="bg-black border w-full h-full rounded-xl">
 				<div className="text-white w-full text-2xl font-bold m-4">
 				<h2>Todo List</h2>
 				</div>
-
+				<div className="space-y-4">
+				{todos.map((todo, id) => (
+					<div key={id} className="flex justify-center bg-gray-400 m-4 rounded-xl">
+						<div className="flex flex-col border m-4">
+							<h4 className="text-sm">{todo.task}</h4>
+						<span className="text-sm">{todo.deadline}</span>
+						</div>
+					</div>
+				))}
+				</div>
 			</div>
 
 			<div className="bg-purple-500 w-full h-full">
