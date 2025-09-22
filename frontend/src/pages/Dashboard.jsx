981fb9c49b5ec4c1986a98ba77c88283e1f4c752
Vikash Singh <vikassingh1999999@gmail.com>
2025-09-23 02:15:23 +0530

    building dashboard p-3

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -4,7 +4,7 @@ export default function Dashboard() {
 	const todos = [
 	  { task: "Complete Assignment", status: "Completed", deadline: "10/11/2023, 12:00 PM" },
 	  { task: "Submit Project", status: "Completed", deadline: "10/11/2023, 5:22 PM" },
-	  { task: "Practice DSA", status: "Ongoing", deadline: "10/11/2023, 7:00 PM" },
+	  { task: "Practice DSA", status: "Pending", deadline: "10/11/2023, 7:00 PM" },
 	  { task: "Fix the bug", status: "Pending", deadline: "10/11/2023, 8:30 PM" },
 	  
 	];
@@ -18,12 +18,18 @@ export default function Dashboard() {
 				</div>
 				<div className="space-y-4">
 				{todos.map((todo, id) => (
-					<div key={id} className="flex justify-center bg-gray-400 m-4 rounded-xl">
-						<div className="flex flex-col border m-4">
-							<h4 className="text-sm">{todo.task}</h4>
-						<span className="text-sm">{todo.deadline}</span>
+					<div key={id} className="flex bg-gray-900 m-4 rounded-xl text-white">
+						<div className="flex flex-col m-4 font-semibold text-md">
+							<h4>{todo.task}</h4>
+						<span className="text-gray-500">{todo.deadline}</span>
 						</div>
-					</div>
+						<div className="flex items-center ml-12">
+							<span className={`py-1.5 px-3 rounded-full text-sm
+								${todo.status === "Completed" ? "bg-green-600" : "bg-red-600"}`}>
+								{todo.status}
+							</span>	
+						</div>
+					</div> 
 				))}
 				</div>
 			</div>
