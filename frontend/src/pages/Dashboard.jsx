c9b973cc0461ede2cac766ea349b1065bcf5471b
Vikash Singh <vikassingh1999999@gmail.com>
2025-09-24 02:50:34 +0530

    building dashboard p-4

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,4 +1,4 @@
-import { Heading } from "../components/Heading"
+import { InputBox } from "../components/InputBox"
 
 export default function Dashboard() {
 	const todos = [
@@ -10,32 +10,39 @@ export default function Dashboard() {
 	];
   
 	return (
-	  <div className="flex bg-gray-200 justify-center itmes-center h-screen p-25">
-		<div className="grid grid-cols-1 md:grid-cols-2 gap-6 w-full h-120">
-			<div className="bg-black border w-full h-full rounded-xl">
-				<div className="text-white w-full text-2xl font-bold m-4">
+		
+	  <div className="flex bg-gray-200 justify-center itmes-center h-screen p-18">
+		<div className="grid grid-cols-1 md:grid-cols-2 gap-6 w-full h-135">
+			<div className="bg-black w-full h-full rounded-xl p-3">
+				<div className="text-white w-full text-4xl font-bold m-4">
 				<h2>Todo List</h2>
 				</div>
 				<div className="space-y-4">
 				{todos.map((todo, id) => (
-					<div key={id} className="flex bg-gray-900 m-4 rounded-xl text-white">
-						<div className="flex flex-col m-4 font-semibold text-md">
+					<div key={id} className="flex bg-gray-900 py-1.5 px-2 mr-5 ml-5 rounded-xl text-white">
+						<div className="flex flex-col w-full p-2 h-20 font-semibold text-lg">
 							<h4>{todo.task}</h4>
 						<span className="text-gray-500">{todo.deadline}</span>
 						</div>
-						<div className="flex items-center ml-12">
-							<span className={`py-1.5 px-3 rounded-full text-sm
+						<div className="flex items-center mr-4 gap-5">
+							<span className={`py-1 px-3 rounded-full text-md font-bold
 								${todo.status === "Completed" ? "bg-green-600" : "bg-red-600"}`}>
 								{todo.status}
 							</span>	
+							<button className="bg-blue-600 py-2 px-5 rounded-xl text-lg">Edit</button>
+							<button className="bg-red-600 py-2 px-5 rounded-xl text-lg">Delete</button>
 						</div>
 					</div> 
 				))}
 				</div>
 			</div>
 
-			<div className="bg-purple-500 w-full h-full">
 				
+			<div className="flex flex-col rounded-xl bg-black">
+				<div className="m-4 text-4xl font-bold text-white"><h2>Add Task</h2></div>
+				<div className="mr-2 ml-2 ">
+					<InputBox placeholder={"Task"}/>
+				</div>
 			</div>
 		</div>
 	  </div>
