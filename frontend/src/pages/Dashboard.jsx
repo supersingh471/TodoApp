2ce27eb04cc7a37080cb9b5a3c5f99550c50c95b
Vikash Singh <vikassingh1999999@gmail.com>
2025-09-25 03:06:05 +0530

    Completed dashboard

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,4 +1,4 @@
-import { InputBox } from "../components/InputBox"
+
 
 export default function Dashboard() {
 	const todos = [
@@ -12,6 +12,7 @@ export default function Dashboard() {
 	return (
 		
 	  <div className="flex bg-gray-200 justify-center itmes-center h-screen p-18">
+		{/*Todo List*/}
 		<div className="grid grid-cols-1 md:grid-cols-2 gap-6 w-full h-135">
 			<div className="bg-black w-full h-full rounded-xl p-3">
 				<div className="text-white w-full text-4xl font-bold m-4">
@@ -29,19 +30,24 @@ export default function Dashboard() {
 								${todo.status === "Completed" ? "bg-green-600" : "bg-red-600"}`}>
 								{todo.status}
 							</span>	
-							<button className="bg-blue-600 py-2 px-5 rounded-xl text-lg">Edit</button>
-							<button className="bg-red-600 py-2 px-5 rounded-xl text-lg">Delete</button>
+							<button className="bg-blue-600 py-2 px-5 rounded-xl text-lg cursor-pointer">Edit</button>
+							<button className="bg-red-600 py-2 px-5 rounded-xl text-lg cursor-pointer">Delete</button>
 						</div>
 					</div> 
 				))}
 				</div>
 			</div>
 
-				
+			{/*Add Task*/}	
 			<div className="flex flex-col rounded-xl bg-black">
-				<div className="m-4 text-4xl font-bold text-white"><h2>Add Task</h2></div>
-				<div className="mr-2 ml-2 ">
-					<InputBox placeholder={"Task"}/>
+				<div className="m-6 text-4xl font-bold text-white"><h2>Add Task</h2></div>
+				<form className="px-8 py-2 flex flex-col gap-6">
+					<input placeholder="task" type="text" className="bg-gray-900 focus:outline-none focus:border-3 text-white border-2 border-purple-500 w-full px-2 py-3 rounded-xl"/>
+					<input placeholder="status" type="text" className="bg-gray-900 focus:outline-none focus:border-3 text-white border-2 border-purple-500 w-full px-2 py-3 rounded-xl"/>
+					<input type="datetime-local" className="bg-gray-900 focus:outline-none focus:border-3 text-white border-2 border-purple-500 w-full px-2 py-3 rounded-xl"/>
+				</form>
+				<div className="m-15 text-white font-bold text-lg">
+					<button className="bg-gradient-to-r from-purple-500 to-pink-600 w-full h-12 rounded-4xl cursor-pointer">Add Task</button>
 				</div>
 			</div>
 		</div>
